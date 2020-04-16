eea8d50d71224203d4164d8873438d5b82a314fb
Kevin Donelson <kevin.donelson@alchemysystems.com>
2020-04-16 08:33:43 -0500

    setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-bucket-2"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://vbtmni3t4d.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_qa5gGARQB",
+    APP_CLIENT_ID: "4sieaua5ujh6ajuplgpreitpnd",
+    IDENTITY_POOL_ID: "us-east-1:eeb2c45b-19d9-45f3-b612-db1dc87207d9"
+  }
+};
